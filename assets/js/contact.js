ec0123dc6b5e7894bae24e07e937019e9ea95ab6
Sanjaya Paudel <paudelsanjaya702@gmail.com>
2022-02-21 20:37:15 +0545

    few pages added with responsive

---
+++ assets/js/contact.js
@@ -0,0 +1,25 @@
+// mobile nav
+$(".nav-tab").on("click", function () {
+  $(".mobile-nav").animate({
+    left: "0px",
+    opacity: "1",
+  });
+  $(this).attr("data-value", "close");
+  $("#contactBody").addClass("fixed-body");
+});
+
+$("#contactBody").on("click", function (e) {
+  const targetEl = e.target;
+  const className = $(targetEl).attr("class")?.split(" ")[0];
+  if (
+    className !== "nav-tab" &&
+    className !== "mobile-nav" &&
+    className !== "mb-link"
+  ) {
+    $(".mobile-nav").animate({
+      left: "-100%",
+      opacity: "0",
+    });
+    $("#contactBody").removeClass("fixed-body");
+  }
+});
